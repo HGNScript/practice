da25625211d115870b67df089ae56af563d447dc
HGN <291908320@qq.com>
2018-05-31 18:02:50 +0800

    '5.31'

--- public/static/practiceTeacher/js/common.js
+++ public/static/practiceTeacher/js/common.js
@@ -1,11 +1,11 @@
 $(function() {
 
-    var __main = function() {
-    	var src = cookies('src');
-    	if (src) {
-	    	$('#iframe').attr("src",src)
-	    	cookies({ src: null });
-    	}
-    }
-    __main();
+    // var __main = function() {
+    // 	var src = cookies('src');
+    // 	if (src) {
+	   //  	$('#iframe').attr("src",src)
+	   //  	cookies({ src: null });
+    // 	}
+    // }
+    // __main();
 })
